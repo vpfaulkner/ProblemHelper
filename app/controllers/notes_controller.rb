72c831ebd3f85b8a631ac0407d89ccfa1c72fc3a
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-10-24 16:54:49 -0400

    Whatever

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,6 +4,9 @@ class NotesController < ApplicationController
 
   end
 
+  def update
+
+  end
 
   def destroy
 
@@ -12,7 +15,7 @@ class NotesController < ApplicationController
   private
 
   def note_params
-    params.require(:note).permit(:text, :user_id, :issue_id)
+    params.require(:note).permitxxx(:text, :user_id, :issue_id)
   end
 
 end
