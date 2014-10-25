249a4b6ae0ebe06ea3c8d073ea413baa059b563d
Maria <mliberov@gmail.com>
2014-10-25 18:11:09 -0400

    added code to notes_controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,15 +1,36 @@
 class NotesController < ApplicationController
+before_action :authenticate
 
   def create
+    @issue = Issue.find(params[:issue_id])
+    @note = @issue.notes.build(notes_params)
+    @note.user = current_user
 
+    if @note.save
+      redirect_to @issue
+    else
+      render "issues/show"
+    end
   end
 
   def update
-
+    respond_to do |format|
+      if @note.update(note_params)
+        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
+#        format.json { render :show, status :ok, location: @note }
+      else
+        format.html { render :edit }
+#        format.json { render json: @note.errors, status :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
-
+    @note.destroy
+    respond_to do |format|
+      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.'}
+      format.json { head :no_content }
+    end
   end
 
   private
