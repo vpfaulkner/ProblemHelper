c2996c0a5e892be2ae4fd6f2d711be211138b8a7
Maria <mliberov@gmail.com>
2014-10-26 20:11:41 -0400

    added notes controller tests

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -2,7 +2,27 @@ require 'test_helper'
 
 class NotesControllerTest < ActionController::TestCase
   include Devise::TestHelpers
-  # test "the truth" do
-  #   assert true
-  # end
+
+    setup do
+      @note = notes(:one)
+    end
+
+    test "should create note" do
+      assert_difference('Note.count') do
+        post :create, note: @note.text
+    end
+
+      assert_redirected_to issue_path(assigns(:issue))
+    end
+
+    test "should update note" do
+      patch :update, id: @issue, note: @note.text
+      assert_redirected_to issue_path(assigns(:issue))
+    end
+
+    test "should destroy note" do
+      assert_difference('Note.count', -1) do
+        delete :destroy, id: @note
+      end
+    end
 end
